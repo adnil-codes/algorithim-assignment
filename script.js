faa6ab5ff5f57b8548852b1ca22af4e65b91ae42
Linda Chukwu <68120237+adnil-codes@users.noreply.github.com>
2021-12-14 11:55:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let array = [ 1, 2, 3, 4 ];
-let array1 = [0,1,2,3,4];
+/*Implement a function, findProduct(), that takes an array of integers, replaces each element of the array with the product of the other elements. 
+let array = [ 1, 2, 3, 4 ] => [24, 12, 8, 6]
+Let array1 = [0,1,2,3,4] => [24, 0, 0, 0]
+ */
  
+const arr = [1, 2, 3, 4];
+
 function findProduct(arr){
-  const sumOfCompArray = (compArray) => {
-    let product = 1
-    for(let num of compArray){
-      product = product * num
-    }
-    return product
-  }
-  
-  let resultingArray = []
-  //[1,2,3,4]
-  for(let num of arr){
-    let complimentArray = arr.filter(a => a !== num)
-    resultingArray.push(sumOfCompArray(complimentArray))
-  }
-  
-  return resultingArray
+  let product = arr.reduce((a,b) => a * b);
+ let newArr = [];
+ 
+ for(let i = 0; i <arr.length; i++) {
+  newArr.push(product/arr[i])
 }
-console.log(array1);
+ return newArr
+ 
+console.log(findProduct(arr));
